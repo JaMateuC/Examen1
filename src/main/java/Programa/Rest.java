9c570ac25476bc1110e9d90246b1c6b795cc5716
JaMateuC <jaumemateu96@gmail.com>
2017-11-13 13:56:52 +0100

    part1

--- src/main/java/Programa/Rest.java
+++ src/main/java/Programa/Rest.java
@@ -3,10 +3,13 @@ package Programa;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 
 @Path("/")
 public class Rest {
+
+    ProductManagerImpl pmi = ProductManagerImpl.getInstance();
 
     @GET
     @Path("/a")
@@ -16,16 +19,21 @@ public class Rest {
     }
 
     @GET
-    @Path("/got/{id}")
+    @Path("/got/{usuario}")
     @Produces(MediaType.APPLICATION_JSON)
-    public int getTrack(@PathParam("id") int id) {
-        return 0;
+    public Response getPedidos(@PathParam("usuario") String usuario) {
+        List<Pedido> lista = pmi.listadoPedidosUsuario(usuario);
+        for(Pedido pedido: lista){
+
+        }
     }
 
     @POST
-    @Path("/new")
+    @Path("/nuevo/Pedido")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response newTrack(int track) {
-        return Response.status(201).entity("Track added in position").build();
+    public Response newTrack(Pedido pedido) {
+
+        pmi.doPedido();
+        return Response.status(201).entity("Pedido anadido").build();
     }
 }
